4d79bc01094eff2bfe6f54575aa60fdc26c08412
alkindiaz <alkindi.ibn.az@gmail.com>
2021-11-13 23:29:49 +0700

    react intro

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from './Header'
+import Footer from './Footer'
+// function Header() {
+//   return (
+//     <div>
+//       <h2>NETFLIX</h2>
+//       <a href="#">Sign In</a>
+//     </div>
+//   )
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Rendering Element</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div>
+            <h2>NETFLIX</h2>
+            <a href="facebook.com">Sign In</a>
+        </div>
+    )
+}
+
+export default Header
